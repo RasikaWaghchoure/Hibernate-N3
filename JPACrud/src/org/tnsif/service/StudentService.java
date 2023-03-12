6489724c869795d8b9d43582977fd47671917ceb
Admin <Admin@DESKTOP-IK4J9N0>
2023-03-12 15:10:30 +0530

    First Commit

--- JPACrud/src/org/tnsif/service/StudentService.java
+++ JPACrud/src/org/tnsif/service/StudentService.java
@@ -1,5 +1,13 @@
 package org.tnsif.service;
 
-public class StudentService {
+import org.tnsif.entities.Student;
+
+public interface StudentService {
+	
+	//abstract method
+	void add(Student student);
+	void update(Student student);
+	void delete(Student student);
+	Student get(int rollno);
 
 }
